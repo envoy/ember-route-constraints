16ff68a92fe89adeb110efe6d5ed005644b14d13
Adolfo Builes <builes.adolfo@gmail.com>
2015-11-20 09:50:38 -0500

    Add generator.

---
+++ blueprints/ember-route-constraints/files/app/route-constraints.js
@@ -0,0 +1,27 @@
+export default function() {
+  // Add your own transition rules like
+  //
+  // this.transition(
+  //   this.toRoute('admin'),
+  //   this.check(function() {
+  //     return this.get('currentUser.isAdmin');
+  //   }),
+  //   this.redirectTo('index')
+  // );
+
+  // this.transition(
+  //   this.toRoute(['foo', 'bar', 'baz']),
+  //   this.check(function() {
+  //     return true;
+  //   }),
+  //   this.redirectTo('index')
+  // );
+
+  // this.transition(
+  //   this.toRoute(/^user*/),
+  //   this.check(function() {
+  //     return this.get('canAccessUser');
+  //   }),
+  //   this.redirectTo('index')
+  // );
+}
