520449b48a605c1da48845567bba01a5f108daef
Esteban Arango Medina <marranoparael31@gmail.com>
2018-04-02 21:28:28 -0500

    Show current version on EmberInspector

--- index.js
+++ index.js
@@ -2,5 +2,10 @@
 
 module.exports = {
   name: 'ember-route-constraints',
-  demoURL: 'http://envoy.github.io/ember-route-constraints/'
+  demoURL: 'http://envoy.github.io/ember-route-constraints/',
+  included: function(app) {
+    this._super.included.apply(this, arguments);
+
+    app.import('vendor/ember-route-constraints/register-version.js');
+  }
 };
---
+++ vendor/ember-route-constraints/register-version.js
@@ -0,0 +1,1 @@
+Ember.libraries.register('Ember Rute Constraints', '0.0.7');
